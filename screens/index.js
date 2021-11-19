8542a558d79674528de89198dd1e2c127f32fb63
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2021-11-19 08:52:58 +0100

    Tab and Navigation Tabs Screens

---
+++ screens/index.js
@@ -0,0 +1,12 @@
+import Home from "./Home";
+import Home from "./Home"
+import Portfolio from "./Portfolio"
+import Market from "./Market"
+import Profile from "./Profile"
+
+export {
+    Home,      
+    Portfolio,
+    Market,
+    Profile
+}
